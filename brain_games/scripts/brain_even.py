6c5e97c9ca346e6355c37bfc1bd46d7906ba0177
closertoreal <ns17143@ya.ru>
2024-06-25 19:38:20 +0300

    4th step

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+
+import prompt
+from random import randint
+from typing import Tuple
+
+
+def check_random(random_number):
+    if random_number % 2 == 0:
+        return True
+    return False
+
+
+def interface() -> Tuple[str, str]:
+    random_number = randint(1, 100)
+    check = check_random(random_number)
+    if check is True:
+        answer = 'yes'
+    else:
+        answer = 'no'
+    return str(random_number), answer
+
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name}!")
+    counter_for_right_answers = 0
+    print(f'Answer \"yes\" if the number is even, otherwise answer \"no\".')
+    while counter_for_right_answers < 3:
+        random_number, text_answer = interface()
+        print(f'\n Question: {random_number}.')
+        player_answer = prompt.string('Your answer: ')
+        if player_answer == text_answer:
+            print('Correct!')
+            counter_for_right_answers += 1
+        else:
+            print('Incorrect!')
+    print(f'Congratulations, {name}')
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    welcome_user()
+
+
+main()
