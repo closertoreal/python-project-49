65b735954da979c83cacf04c181ef6505cc1b800
closertoreal <ns17143@ya.ru>
2024-06-26 22:46:20 +0300

    7th step

---
+++ brain_games/scripts/games/brain_gcd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from brain_games.scripts.games.engine import brain_interface
+
+
+def main():
+    game_type = 'brain_gcd'
+    brain_interface.main(game_type)
+
+
+if __name__ == '__main__':
+    main()
--- brain_games/scripts/games/engine/brain_interface.py
+++ brain_games/scripts/games/engine/brain_interface.py
@@ -21,22 +21,33 @@ def calculate_expression(number_1, number_2, operator) -> int:
 
 
 def calculations(game_type):
-    if game_type == 'brain_even':
-        random_number = randint(1, 100)
-        check = check_random(random_number)
-        if check is True:
-            right_answer = 'yes'
-        else:
-            right_answer = 'no'
-        return str(random_number), right_answer
-    if game_type == 'brain_calc':
-        list_operators = ['+', '-', '*']
-        operator = random.choice(list_operators)
-        number_1 = random.randint(1, 100)
-        number_2 = random.randint(1, 100)
-        expression = str(number_1) + operator + str(number_2)
-        right_answer = calculate_expression(number_1, number_2, operator)
-        return expression, right_answer
+    match game_type:
+        case 'brain_even':
+            random_number = randint(1, 100)
+            check = check_random(random_number)
+            if check is True:
+                right_answer = 'yes'
+            else:
+                right_answer = 'no'
+            expression = str(random_number)
+        case 'brain_calc':
+            list_operators = ['+', '-', '*']
+            operator = random.choice(list_operators)
+            number_1 = random.randint(1, 100)
+            number_2 = random.randint(1, 100)
+            expression = str(number_1) + operator + str(number_2)
+            right_answer = calculate_expression(number_1, number_2, operator)
+        case 'brain_gcd':
+            number_1 = random.randint(1, 100)
+            number_2 = random.randint(1, 100)
+            expression = str(number_1) + ' ' + str(number_2)
+            counter = min(number_1, number_2)
+            right_answer = 0
+            while counter > 0 and right_answer == 0:
+                if number_1 % counter == 0 and number_2 % counter == 0:
+                    right_answer = counter
+                counter -= 1
+    return expression, right_answer
 
 
 def game_question(game_type):
@@ -44,6 +55,8 @@ def game_question(game_type):
         game_question = 'Answer \"yes\" if the number is even, otherwise answer \"no\".'
     if game_type == 'brain_calc':
         game_question = 'What is the result of the expression?'
+    if game_type == 'brain_gcd':
+        game_question = 'Find the greatest common divisor of given numbers.'
     return game_question
 
 
@@ -55,12 +68,12 @@ def engine(game_type):
     print(game_question(game_type))
     while counter_for_right_answers < 3:
         expression, right_answer = calculations(game_type)
-        print(f'\n Question: {expression}.')
+        print(f'\nQuestion: {expression}.')
         player_answer = prompt.string('Your answer: ')
         if str(player_answer) == str(right_answer):
             print('Correct!')
             counter_for_right_answers += 1
-        if game_type == 'brain_calc':
+        if game_type == 'brain_calc' or game_type == 'brain_gcd':
             if str(player_answer) != str(right_answer):
                 print(f'{str(player_answer)} is wrong answer ;(. Correct answer was {str(right_answer)}.'
                       f'Let\'s try again, {name}!')
