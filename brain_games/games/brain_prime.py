36d7508ad2f1fc22003cb6eded0d95447524cae4
closertoreal <ns17143@ya.ru>
2024-07-03 14:08:16 +0300

    fixed linter's warnings in is_prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -4,12 +4,12 @@ QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(expression):
-  if expression < 2:
-      return True
-  for i in range(2, int(expression ** 0.5) + 1):
-      if expression % i == 0:
-          return False
-  return True
+    if expression < 2:
+        return True
+    for i in range(2, int(expression ** 0.5) + 1):
+        if expression % i == 0:
+            return False
+    return True
 
 
 def create_round_data():
